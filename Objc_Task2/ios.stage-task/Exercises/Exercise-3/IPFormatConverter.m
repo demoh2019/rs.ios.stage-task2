541db26df41c2f632872f6067ba221d32f980d2c
demoh2019 <demoh629@gmail.com>
2021-05-16 08:40:27 +0300

    add dunctional

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,26 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    if(numbersArray == nil || [numbersArray count] == 0){
+        return @"";
+    }
+    NSString *errorRange = @"The numbers in the input array can be in the range from 0 to 255.";
+    NSString *errorNegative = @"Negative numbers are not valid for input.";
+    NSMutableString *result = [[NSMutableString alloc] init];
+    for(NSInteger i = 0; i<4; i++){
+        NSInteger number = 0;
+        if(i < [numbersArray count]){
+            number = [numbersArray[i] integerValue];
+        }
+        if(number > 255){
+            return errorRange;
+        }
+        if(number < 0){
+            return errorNegative;
+        }
+        [result appendFormat:@"%ld.", number];
+    }
+    return [result substringToIndex:[result length]-1];
 }
 
 @end
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,30 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    if([array count] == 0 || array == nil || ![array[0] isKindOfClass:[NSArray class]]){
+        return @[];
+    }
+    NSMutableArray *number = [[NSMutableArray alloc] init];
+    NSMutableArray *string = [[NSMutableArray alloc] init];
+    for(id item in array){
+        for(id el in item){
+            if([item isKindOfClass:NSString.class]){
+                [string addObject:el];
+            }else{
+                [number addObject:el];
+            }
+        }
+    }
+
+    [number sortUsingSelector:@selector(compare:)];
+    [string sortUsingSelector:@selector(compare:)];
+    if(number.count==0){
+        return string;
+    }else if (string.count == 0){
+        return number;
+    }
+    
+    return [NSArray arrayWithObjects:number,string, nil];
 }
 
 @end
